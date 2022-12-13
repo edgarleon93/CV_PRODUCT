ba2196d73e92174162f818fcd03bcf3ec7e8144c
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2022-12-13 16:06:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-
+// Funtion to toggle the nav menu at mobile
+(function toggleNav() {
+    // Creating the variables
+    const navMenu = document.querySelector(".menu");
+    const navBox = document.querySelector(".nav-links");
+    const closeNav = document.querySelector("#nav-close-btn");
+    const navOverlay = document.querySelector(".overlay-nav");
+  
+    navMenu.addEventListener("click", (event) => {
+      navBox.classList.add("show-nav");
+      navOverlay.classList.add("show-overlay");
+    });
+  
+    closeNav.addEventListener("click", (event) => {
+      navBox.classList.remove("show-nav");
+      navOverlay.classList.remove("show-overlay");
+    });
+  })();
+  
+  
+ 
